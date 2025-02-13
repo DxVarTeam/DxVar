2827a95a73903dd38c1b311ab8e75bbc78d990b9
DxVar <dxvar.user2@gmail.com>
2025-02-14 00:57:48 +0400

    Update App.py

--- App.py
+++ App.py
@@ -37,7 +37,7 @@ logo_url = "https://raw.githubusercontent.com/DxVar-1/app/main/dxvarlogo.png"
 st.title("DxVar")
 
 #Sidebar
-language = st.sidebar.selectbox("🌍 Select Language", ["English", "Arabic"])
+language = st.sidebar.radio("🌍 Select Language", ["English", "Arabic"])
 # Store language preference in session state
 st.session_state["language"] = language
 

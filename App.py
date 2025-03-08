6ccf65051a37d77bb8b7ee24d1d399081db2a7ea
DxVar <dxvar.user2@gmail.com>
2025-03-08 18:20:33 +0400

    Update App.py

--- App.py
+++ App.py
@@ -135,7 +135,7 @@ if language == "Arabic":
 
 #ALL FUNCTIONS
 def get_and_dump_pubmed_papers():
-  pmid_query = [pmids, [st.session_state.last_input_ph]]  # Replace with your actual PMID
+  pmid_query = [st.session_state.pmids, [st.session_state.last_input_ph]]  # Replace with your actual PMID
   output_filepath = "paper.jsonl"
   get_and_dump_pubmed_papers(pmid_query, output_filepath='papers.json')
   with open('papers.json', "r", encoding="utf-8") as file:

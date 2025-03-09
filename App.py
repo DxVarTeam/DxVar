9d8598b3dd5785ab003290c5f1614d5b61202a82
DxVar <dxvar.user2@gmail.com>
2025-03-09 12:38:07 +0400

    Update App.py

--- App.py
+++ App.py
@@ -581,7 +581,8 @@ if (user_input != st.session_state.last_input or user_input_ph != st.session_sta
         The following papers were found to be linked with the requested variant the and phenotype (disease) in interest ({st.session_state.last_input_ph}): {filtered_papers}. 
         Analyze the abstracts of the papers then explain and draw a conclusion on if the variant is likely to cause {st.session_state.last_input_ph} or not.
         Whenever providing conclusions or insights, mention which papers were used to draw those conclusions by referencing them using IEEE style like [1].
-        ensure this is done based on the order of the provided papers. Example if 8 papers were used and papers 2 and 5 were referenced write [2][5]"""
+        ensure this is done based on the order of the provided papers. Example if 8 papers were used and papers 2 and 5 were referenced write [2][5]
+        No need to mention the references again at the end, and no need to mention their titles for referencing purposes"""
         
         st.session_state.reply = get_assistant_response_1(user_input_1)
 
